5ee3db2a453447b7936fda96dbac4ab297a630ea
pablo <pablo.barrabes@fppiramide.com>
2024-10-03 10:15:50 +0200

    a clase

---
+++ UD2/ejerciciosUD2.js
@@ -0,0 +1,19 @@
+/* Ejercicio 1 :Declara 6 variables a las que asignaremos los siguientes valores. 1357, 135.7, 135e7, 0b1010, 0o1357 y 0x1A57. 
+Una vez creadas muestra por consola los valores almacenados y el tipo de dato que nos indica el operador typeof. */
+let numero1= 1357;
+let numero2= 135.7;
+let numero3= 135e7;
+let numero4= 0b1010;
+let numero5=0o1357;
+let numero6=0x1A57;
+
+console.log("numero 1:"+ numero1+ "tipo:" +typeof numero1);
+console.log("numero 2:"+ numero2+ "tipo:" +typeof numero2);
+console.log("numero 3:"+ numero3+ "tipo:" +typeof numero3);
+console.log("numero 4:"+ numero4+ "tipo:" +typeof numero4);
+console.log("numero 5:"+ numero5+ "tipo:" +typeof numero5);
+console.log("numero 6:"+ numero6+ "tipo:" +typeof numero6);
+
+/* Ejercicio 2 Pide al usuario un  número utilizando el método prompt(), guarda ese dato en una variable. 
+OJO guardarlo de manera que sea un tipo  Number no un String. 
+Compruébalo mostrando por consola el tipo de dato guardado  con el operador typeof. */
