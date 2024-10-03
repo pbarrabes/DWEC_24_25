b78db3f5d4b99c942543d081bcc9f2f65317df63
pablo <pablo.barrabes@fppiramide.com>
2024-10-03 09:07:02 +0200

    first commit

---
+++ UD1/holamundo1.js
@@ -0,0 +1,4 @@
+document.write("<p>Parrafo añadido con document.write 1</p>")
+document.body.innerHTML="<p>parrafo añadido con innerHtml</p>";
+console.log("Estas aqui");
+console.error("Mensaej de error personalizado");
